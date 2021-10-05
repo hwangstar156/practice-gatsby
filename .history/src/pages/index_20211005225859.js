487993d89cbe5ed467fd9634483c986a0a80ea26
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-10-05 23:34:42 +0900

    learn gatsby-transformer-remark to markddown and gatsby-node

--- .history/src/pages/index_20211005225859.js
+++ .history/src/pages/index_20211005225859.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,14 +11,6 @@ const IndexPage = ({ data }) => (
     <div>
       <h1>Jungmin's twit</h1>
       <h4>{data.allMarkdownRemark.totalCount}</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
     </div>
   </Layout>
 )
--- .history/src/pages/index_20211005225921.js
+++ .history/src/pages/index_20211005225921.js
@@ -10,14 +10,6 @@ const IndexPage = ({ data }) => (
     <div>
       <h1>Jungmin's twit</h1>
       <h4>{data.allMarkdownRemark.totalCount}</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
     </div>
   </Layout>
 )
